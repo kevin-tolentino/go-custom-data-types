6ef19ca0a3fb76f58028228516a603ea433a0374
kevin-tolentino <kevin.tolentino95@gmail.com>
2021-03-17 16:26:22 -0700

    added method to change state of struct and add a twitter handler to Person object

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"go-custom-data-types/organization"
 )
 
 func main() {
 	p := organization.NewPerson("Kevin", "Tolentino")
+	err := p.SetTwitterHandler("@kev_tol")
+	if err != nil {
+		fmt.Printf("an error occured setting twitter handler: %s\n", err.Error())
+	}
+	println(p.TwitterHAndler())
 	println(p.ID())
 	println(p.FullName())
-	//println(p.FullName())
+	println(p.FullName())
 }
--- organization/person.go
+++ organization/person.go
@@ -1,14 +1,19 @@
 package organization
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 type Identifiable interface {
 	ID() string
 }
 
 type Person struct {
-		firstName string
-		lastName string
+	firstName      string
+	lastName       string
+	twitterHandler string
 }
 
 //create a new function called new plus the type
@@ -19,11 +24,26 @@ func NewPerson(firstName, lastName string) Person {
 	}
 }
 
-func (p Person) FullName() string {
+func (p *Person) FullName() string {
 	return fmt.Sprintf("%s %s", p.firstName, p.lastName)
 
 }
 
-func (p Person) ID() string { //implicit implementation of interface of type Identifiable
+func (p *Person) ID() string { //implicit implementation of interface of type Identifiable
 	return "12345"
+}
+
+func (p *Person) SetTwitterHandler(handler string) error {
+	if len(handler) == 0 {
+		p.twitterHandler = handler
+	} else if !strings.HasPrefix(handler, "@") {
+		return errors.New("twitter handler must start with an @ symbol")
+	}
+
+	p.twitterHandler = handler
+	return nil
+}
+
+func (p *Person) TwitterHAndler() string {
+	return p.twitterHandler
 }
